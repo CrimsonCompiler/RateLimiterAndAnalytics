b5513744d6a7ac09751176f47386b42030097941
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-16 20:17:47 +0600

    feat(middleware): add rate limiting middleware to log request IPs and timestamps

---
+++ RateLimiterAndAnalytics/RateLimitingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RateLimiterAndAnalytics
+{
+    public class RateLimitingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RateLimitingMiddleware(RequestDelegate next) { _next = next; }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var ipaddr = context.Connection.RemoteIpAddress?.ToString() ?? "Unknown ip address.";
+            Console.WriteLine($"[GATEKEEPER] Request IP: {ipaddr} |  Time: {DateTime.Now:HH:mm:ss}");
+            await _next(context);
+        }
+    }
+}
