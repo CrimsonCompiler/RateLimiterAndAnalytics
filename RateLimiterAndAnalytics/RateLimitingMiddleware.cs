27c4ef4eab654e56c93bca5a85ab71f6a04708ab
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-16 21:15:35 +0600

    feat(middleware): enhance rate limiting logic to include request counting and reset mechanism

--- RateLimiterAndAnalytics/RateLimitingMiddleware.cs
+++ RateLimiterAndAnalytics/RateLimitingMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Collections.Concurrent;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -9,12 +10,42 @@ namespace RateLimiterAndAnalytics
     {
         private readonly RequestDelegate _next;
 
+        // Counting the ip using Dictionary <string, int>
+        private static readonly ConcurrentDictionary<string, int> _requestCount = new();
+
+        // Timer: when the timer will hit 0
+        private static DateTime _resetTime = DateTime.Now.AddMinutes(1);
+
         public RateLimitingMiddleware(RequestDelegate next) { _next = next; }
+
+
+
 
         public async Task InvokeAsync(HttpContext context)
         {
+
+            if(DateTime.Now > _resetTime)
+            {
+                _requestCount.Clear();
+                _resetTime = DateTime.Now.AddMinutes(1);
+                Console.WriteLine("[SYSTEM] Rate limit log is cleared");
+            }
+
+
             var ipaddr = context.Connection.RemoteIpAddress?.ToString() ?? "Unknown ip address.";
-            Console.WriteLine($"[GATEKEEPER] Request IP: {ipaddr} |  Time: {DateTime.Now:HH:mm:ss}");
+
+            var currentCount = _requestCount.AddOrUpdate(ipaddr, 1, (key, count) => count + 1);
+
+            Console.WriteLine($"[GATEKEEPER] IP: {ipaddr} | Hit Count: {currentCount}");
+
+
+            if(currentCount > 5)
+            {
+                Console.WriteLine($"[Blocked] IP {ipaddr} is blocked");
+                context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                await context.Response.WriteAsync("Too many requests. Please wait a minute and try again.");
+                return; 
+            }
             await _next(context);
         }
     }
