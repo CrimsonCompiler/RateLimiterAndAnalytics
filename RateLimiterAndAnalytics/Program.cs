169f0c067328ffaa8426de5b075e3037bff9aa27
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-18 13:14:49 +0600

    feat(analytics): add analytics route to retrieve request statistics from the database

--- RateLimiterAndAnalytics/Program.cs
+++ RateLimiterAndAnalytics/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 
 namespace RateLimiterAndAnalytics
@@ -37,6 +38,41 @@ namespace RateLimiterAndAnalytics
                 students.Add(newStudent.Name);
                 return Results.Ok(students);
             });
+
+
+            //Analytics Route
+            
+            app.MapGet("/api/analytics", async () =>
+            {
+                string connectionString = "Server=DESKTOP-4915MGS\\SQLEXPRESS;Database=ApiAnalyticsDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+                var dashboardData = new Dictionary<string, object>();
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    await conn.OpenAsync();
+
+                    using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM RequestLogs", conn))
+                    {
+                        dashboardData["TotalRequests"] = await cmd.ExecuteScalarAsync();
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM RequestLogs WHERE IsBlocked = 1", conn))
+                    {
+                        dashboardData["TotalBlocked"] = await cmd.ExecuteScalarAsync();
+                    }
+
+                    string topIpQuery = "SELECT TOP 1 IPAddress FROM RequestLogs GROUP BY IPAddress ORDER BY COUNT(*) DESC";
+                    using (SqlCommand cmd = new SqlCommand(topIpQuery, conn))
+                    {
+                        var topIp = await cmd.ExecuteScalarAsync();
+                        dashboardData["TopSpammerIP"] = topIp ?? "None";
+                    }
+                }
+
+                return Results.Ok(dashboardData);
+            });
+
 
             app.Run();
 
