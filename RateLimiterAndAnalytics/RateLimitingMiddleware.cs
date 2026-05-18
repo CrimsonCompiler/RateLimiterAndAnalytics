c49dccd80cd0d943abfe576741c4e02113e86a88
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-18 13:01:03 +0600

    feat(middleware): enhance rate limiting middleware to log request details to the database

--- RateLimiterAndAnalytics/RateLimitingMiddleware.cs
+++ RateLimiterAndAnalytics/RateLimitingMiddleware.cs
@@ -17,10 +17,10 @@ namespace RateLimiterAndAnalytics
         // Timer: when the timer will hit 0
         private static DateTime _resetTime = DateTime.Now.AddMinutes(1);
 
+
+        private readonly string _connectString = "Server=DESKTOP-4915MGS\\SQLEXPRESS;Database=ApiAnalyticsDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
         public RateLimitingMiddleware(RequestDelegate next) { _next = next; }
-
-
-
 
         public async Task InvokeAsync(HttpContext context)
         {
@@ -34,13 +34,15 @@ namespace RateLimiterAndAnalytics
 
 
             var ipaddr = context.Connection.RemoteIpAddress?.ToString() ?? "Unknown ip address.";
-
             var currentCount = _requestCount.AddOrUpdate(ipaddr, 1, (key, count) => count + 1);
 
-            Console.WriteLine($"[GATEKEEPER] IP: {ipaddr} | Hit Count: {currentCount}");
+            bool isBlocked = currentCount > 5;
+
+            // Sending log to database in background
+            _ = LogRequestToDatabaseAsync(ipaddr, isBlocked);
 
 
-            if(currentCount > 5)
+            if(isBlocked)
             {
                 Console.WriteLine($"[Blocked] IP {ipaddr} is blocked");
                 context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
@@ -48,6 +50,32 @@ namespace RateLimiterAndAnalytics
                 return; 
             }
             await _next(context);
+
+        }
+
+        private async Task LogRequestToDatabaseAsync(string ip, bool isBlocked)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_connectString))
+                {
+                   await conn.OpenAsync();
+
+                    // INSERT COMMAND
+                    string query = "INSERT INTO RequestLogs(IPAddress, IsBlocked) VALUES(@ip, @blocked)";
+
+                    using(SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@ip", ip);
+                        cmd.Parameters.AddWithValue("@blocked", isBlocked);
+                        await cmd.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[DB ERROR] Log save failed: {ex.Message}");
+            }
         }
     }
 }
